a070c9f6061ad2882ca24218671d81b67d2cda73
Hansen <646380243@qq.com>
2022-05-02 23:27:39 +0800

    算法

---
+++ Demo/react/react-route6/08-src-state参数/pages/Detail.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+export default function Detail() {
+    // 用useLocation就可以获取参数
+    const { state: {id, title, content} } = useLocation()
+    return (
+        <ul>
+            <li>消息编号: {id}</li>
+            <li>消息标题: {title}</li>
+            <li>消息内容: {content}</li>
+        </ul>
+    )
+}
--- Demo/react/react-route6/08-src-state参数/pages/Message.jsx
+++ Demo/react/react-route6/08-src-state参数/pages/Message.jsx
@@ -1,26 +1,13 @@
 import React, { useState } from 'react'
-import { Link, Outlet, useNavigate } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 export default function Message() {
-    const navigate = useNavigate()
     const [messages] = useState([
         {id: '0001', title: '消息1', content: '白日依山尽'},
         {id: '0002', title: '消息2', content: '黄河入海流'},
         {id: '0003', title: '消息3', content: '欲穷千里目'},
         {id: '0004', title: '消息4', content: '更上一层楼'}
     ])
-
-    function showDetail(m) {
-        navigate('detail', {
-            replace: false,
-            // 这边只能写state，不能写search和params
-            state: {
-                id: m.id,
-                title: m.title,
-                content: m.content
-            }
-        })
-    }
     return (
         <div>
             <ul>
@@ -37,7 +24,6 @@ export default function Message() {
                                         content: m.content
                                     }}
                                 >{m.title}</Link>
-                                <button onClick={() => showDetail(m)}>查看详情</button>
                             </li>
                         )
                     })
