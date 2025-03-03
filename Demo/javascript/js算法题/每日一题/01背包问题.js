3be71a828ffd0f8206cf1ac052b73d5139b9e0c1
金汉生 <jinhansheng@gonglu.info>
2025-03-03 17:51:30 +0800

    update

---
+++ Demo/javascript/js算法题/每日一题/01背包问题.js
@@ -0,0 +1,31 @@
+
+// 这种挺好的，就是逆行推断，但是我们今天用一种自己比较好理解的方式
+// const package = (bagWeight, value, weight) => {
+//   const dp = new Array(bagWeight + 1).fill(0);
+//   for (let i = 0; i < weight.length; i++) {
+//     for (let j = bagWeight; j >= weight[i]; j--) {
+//       dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
+//     }
+//   }
+//   return dp[bagWeight];
+// }
+
+const package = (bagWeight, value, weight) => {
+    let result = new Array(bagWeight + 1).fill(0);
+    for (let i = 0; i < value.length; i++) { // 控制物品行数
+        const next = [];
+        for (let j = 0; j <= bagWeight; j++) { // 控制列数
+            if (j >= weight[i]) {
+                next[j] = Math.max(result[j], result[j - weight[i]] + value[i]); // result代表的是上一行的
+            } else {
+                next[j] = result[j];
+            }
+        }
+        result = next;
+    }
+    return result[bagWeight];
+
+}
+
+
+console.log(package(6, [5, 10, 3, 6, 3], [2, 5, 1, 4, 3]));
